d82a54c5828a6f5c948ddf7b5b3130b362fa1675
Franz Gollhammer <franz.gollhammer@gmail.com>
2018-03-19 23:05:23 +0100

    Added API and axios to proect for fetching album data

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create Component
 const App = () => (
-    <Header headerText={'albums'} /> 
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList /> 
+  </View>
   );
 
 // Render to device
