6e183640d069070ebebbc23816f41c29035da29a
Franz Gollhammer <franz.gollhammer@gmail.com>
2018-03-22 18:31:24 +0100

    Added new ScrollView, Added Button for Buy Album Now with a link to amazon.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import AlbumList from './src/components/AlbumList';
 
 // Create Component
 const App = () => (
-  <View>
+  <View style={{ flex: 1 }}>
     <Header headerText={'Albums'} />
     <AlbumList /> 
   </View>
